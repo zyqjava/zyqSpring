d1e2fbed3af6b33ee69b7b35c9e49aeb0e2caa06
544741562@qq.com <@zhang1994>
2021-05-21 17:24:52 +0800

    实例化

--- src/main/java/com/zyqSpring/springframework/beans/support/ZyqBeanDefinitionReader.java
+++ src/main/java/com/zyqSpring/springframework/beans/support/ZyqBeanDefinitionReader.java
@@ -35,7 +35,7 @@ public class ZyqBeanDefinitionReader {
      */
     public ZyqBeanDefinitionReader(String... locations) {
         try ( //定位，通过url定位找到配置文件，然后转换成文件流
-              InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(locations[0].replace("classpath:", ""));) {
+              InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(locations[0].replace("classpath:", ""))) {
         //加载，保存为properties
             config.load(inputStream);
         } catch (Exception e) {
